37df050fb66b83a7728091f758a9f0f5b07c8532
Felicianna.DeSantos <Felicianna.DeSantos@MU-C1-06.mullen.pvt.k12.co.us>
2026-02-23 15:01:14 -0700

    initial commit

---
+++ SIS Project/src/Menus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Menus
+	{
+		static Scanner userIntInput = new Scanner(System.in);
+		public static void main(String[] args)
+		{
+		   displayMenu();
+	}
+		private static void displayMenu()
+		{
+			System.out.println("What would you like to do?");
+			System.out.println("1. Add or delete a student");
+		}
